c6ffe9f302f2e70858bd7fdd2463b2d55960d62e
Oussamakrabchi <106429345+Oussamakrabchi@users.noreply.github.com>
2023-01-04 20:44:34 +0100

    Add files via upload

---
+++ Untitled18.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[35]:
+
+
+mylist=[14, "hello", 967]
+mylist[2] # we only have 3 elements in the list so we can only chose 0-1-2
+
+
+# In[36]:
+
+
+import pandas as pd
+import numpy as np
+
+
+# In[37]:
+
+
+print("python errors") #adding the ()
+
+
+# In[38]:
+
+
+mydictionnary={True:"hello",False:"bye", '3':"python"}
+mydictionnary[True] #removing the ''
+
+
+# In[39]:
+
+
+i=14
+while i<78:
+    #print index
+    print(i)
+i+=1
+
+
+# In[41]:
+
+
+
+it=iter([1,2,3])
+next(it)
+next(it)
+next(it)
+next(it)
+
+
+# In[21]:
+
+
+15+15
+
+
+# In[42]:
+
+
+int('python') # cant turn a string into integer
+
+
+# In[44]:
+
+
+python = "python" #python was not defined 
+
+
+# In[45]:
+
+
+x= 19/1 #cant devise by 0
+
+
+# In[ ]:
+
+
+
+
